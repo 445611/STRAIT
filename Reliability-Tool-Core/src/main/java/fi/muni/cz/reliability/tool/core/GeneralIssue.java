b161d7ecf6f641d548beb48ee4bcc56e619a0045
445611 <445611@mail.muni.cz>
2018-03-22 18:16:17 +0100

    project initialization

---
+++ Reliability-Tool-Core/src/main/java/fi/muni/cz/reliability/tool/core/GeneralIssue.java
@@ -0,0 +1,8 @@
+package fi.muni.cz.reliability.tool.core;
+
+/**
+ * @author Radoslav Micko <445611@muni.cz>
+ */
+public class GeneralIssue {
+    
+}
---
+++ Reliability-Tool-Utils/src/main/java/fi/muni/cz/reliability/tool/utils/IssuesProcessor.java
@@ -0,0 +1,8 @@
+package fi.muni.cz.reliability.tool.utils;
+
+/**
+ * @author Radoslav Micko <445611@muni.cz>
+ */
+public interface IssuesProcessor {
+    
+}
