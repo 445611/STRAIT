38ff480999799188b89610f65858dcc7dd7da80b
445611 <445611@mail.muni.cz>
2018-03-22 01:47:07 +0100

    Init commit

---
+++ Reliability-Tool-DataProvider/src/main/java/fi/muni/cz/reliability/tool/dataprovider/DataProvider.java
@@ -0,0 +1,8 @@
+package fi.muni.cz.reliability.tool.dataprovider;
+
+/**
+ * @author Radoslav Micko <445611@muni.cz>
+ */
+public interface DataProvider {
+    
+}
---
+++ Reliability-Tool-Models/src/main/java/fi/muni/cz/reliability/tool/models/Model.java
@@ -0,0 +1,8 @@
+package fi.muni.cz.reliability.tool.models;
+
+/**
+ * @author Radoslav Micko <445611@muni.cz>
+ */
+public interface Model {
+    
+}
